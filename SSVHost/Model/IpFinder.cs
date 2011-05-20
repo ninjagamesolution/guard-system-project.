86284e6a5cd335399344f6e6033346c497d748c4
ExpertBC <91556074+ExpertBC@users.noreply.github.com>
2011-05-20 00:02:00 -0700

    Update

---
+++ SSVHost/Model/IpFinder.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SSVHost.Model
+{
+    public class IpFinder
+    {
+        public string FindIpAddressByMacAddress()
+        {
+            List<ArpEntity> arpEntities = GetArpResult();
+            string IP = GetIPAddress().ToString();
+
+            string str_arp = IP + "#server;";
+            foreach (var arpitem in arpEntities)
+            {
+                if (arpitem.Type == "static") continue;
+                if (arpitem.Ip.Split('.')[2] != IP.Split('.')[2]) continue;
+
+                str_arp += $"{arpitem.Ip}#client;";
+                //Console.WriteLine($"{arpitem.Ip}  {arpitem.MacAddress}   {arpitem.Type}");
+            }
+            return str_arp;
+        }
+
+        public List<ArpEntity> GetArpResult()
+        {
+            var p = Process.Start(new ProcessStartInfo("arp", "-a")
+            {
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardOutput = true
+            });
+
+            var output = p?.StandardOutput.ReadToEnd();
+            p?.Close();
+
+            return ParseArpResult(output);
+        }
+
+        private List<ArpEntity> ParseArpResult(string output)
+        {
+            var lines = output.Split('\n').Where(l => !string.IsNullOrWhiteSpace(l));
+
+            var result =
+                (from line in lines
+                 select Regex.Split(line, @"\s+")
+                    .Where(i => !string.IsNullOrWhiteSpace(i)).ToList()
+                    into items
+                 where items.Count == 3
+                 select new ArpEntity()
+                 {
+                     Ip = items[0],
+                     MacAddress = items[1],
+                     Type = items[2]
+                 });
+
+            return result.ToList();
+        }
+
+        public static IPAddress GetIPAddress()
+        {
+            IPAddress m_IPAddress = null;
+            foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (ni.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 || ni.NetworkInterfaceType == NetworkInterfaceType.Ethernet)
+                {
+                    if (ni.Name.StartsWith("Ethernet"))
+                    {
+                        foreach (UnicastIPAddressInformation ip in ni.GetIPProperties().UnicastAddresses)
+                        {
+                            if (ip.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                            {
+                                m_IPAddress = ip.Address;
+                                break;
+                            }
+                        }
+                    }
+
+                }
+            }
+            return m_IPAddress;
+        }
+    }
+
+    public class ArpEntity
+    {
+        public string Ip { get; set; }
+
+        public string MacAddress { get; set; }
+
+        public string Type { get; set; }
+    }
+}
---
+++ TaskControl/myEvents/EventHandlers.cs
@@ -0,0 +1,134 @@
+﻿using Monitor.TaskControl.Globals;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Reflection;
+using System.Windows;
+using Monitor.TaskControl.Logger;
+using Monitor.TaskControl.View;
+using Monitor.TaskControl.Models;
+using System.Net.Sockets;
+using Monitor.TaskControl.Communication;
+
+namespace Monitor.TaskControl.myEvents
+{
+    class EventHandlers
+    {
+        static EventHandlers()
+        {
+            AppDomain.CurrentDomain.UnhandledException += OnUnhandledException;
+            AppDomain.CurrentDomain.AssemblyResolve += OnAssemblyResolve;
+
+            Events.OnStartUp += OnStartUp;
+            Events.OnExit += OnExit;
+            Events.OnRegister += OnRegister;
+            Events.OnReceiveData += OnReceiveData;
+            Events.OnPassword += OnPassword;
+            Events.OnMainProc += OnMainProc;
+
+        }
+
+        public static void Initialize()
+        {
+        }
+
+        private static void OnUnhandledException(object sender, UnhandledExceptionEventArgs args)
+        {
+            var exception = (Exception)args.ExceptionObject;
+
+            if (args.IsTerminating)
+            {
+                //                Settings.Save();
+            }
+
+            //            Log.Instance.DoLog(string.Format("Unhandled Exception.\r\nException: {0}\r\n", exception), Log.LogType.Error);
+
+        }
+
+        private static Assembly OnAssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            //foreach (var file in Directory.GetFiles(Settings.Instance.Directories.DependenciesDirectory))
+            //{
+            //    if (Path.GetFileNameWithoutExtension(file) == (new AssemblyName(args.Name).Name))
+            //    {
+            //        return Assembly.LoadFrom(file);
+            //    }
+            //}
+
+            return null;
+        }
+
+        private static void OnStartUp(StartupEventArgs startupEventArgs)
+        {
+            Log.Instance.DoLog("-------- OnStartup ----", Log.LogType.Info);
+            //Settings.Instance.Directories.CreateDirectories();
+            if (!Settings.Instance.RegValue.ExistsGetValue())
+            {
+
+                Windows.LoginWindow = new LoginWindow();
+                Windows.LoginWindow.Show();
+                //Settings.Instance.Directories.CreateDirectories();
+                //Communications.StartUpSocket();
+            }
+            else
+            {
+                Settings.Instance.Directories.CreateDirectories();
+                Windows.MainWindow = new MainWindow();
+                Windows.MainWindow.Show();
+                Communications.StartUpSocket();
+            }
+        }
+
+        private static void OnReceiveData(byte[] buff, int len, string ClientIP)
+        {
+            CommProc.Instance.RecDataAnalysis(buff, len, ClientIP);
+        }
+
+
+        private static void OnPassword(EventArgs exitEventArgs)
+        {
+            Settings.Instance.RegValue.Password = Windows.LoginWindow.PasswordTextBox.Password;
+            Settings.Instance.RegValue.WriteValue();
+            //Settings.Instance.RegValue.BaseDirectory = Constants.BaseDirectory;
+            Settings.Instance.RegValue.WriteValue();
+            Settings.Instance.Directories.CreateDirectories();
+            if (Windows.LoginWindow != null)
+            {
+                Windows.LoginWindow.Hide();
+            }
+
+            Windows.MainWindow = new MainWindow();
+            Windows.MainWindow.Show();
+        }
+
+        private static void OnExit(ExitEventArgs exitEventArgs)
+        {
+            Log.Instance.DoLog("TaskView is exiting.");
+            Windows.MainWindow.DeleteTryIcon();
+            //// Cleanup
+
+            //           Settings.Save();
+
+            Log.Instance.DoLog("TaskView has exited.\r\n");
+        }
+
+        private static void OnRegister(EventArgs exitEventArgs)
+        {
+            Settings.Instance.RegValue.BaseDirectory = Windows.LoginWindow.WorkDirectory.Text;
+            Settings.Instance.RegValue.WriteValue();
+            Settings.Instance.Directories.CreateDirectories();
+            Communications.StartUpSocket();
+            //Communications.StartUpSocket();
+            //Settings.Instance.RegValue.ServerIP = Windows.LoginWindow.ServerIP.Text;
+            //Settings.Instance.RegValue.UserName = Windows.LoginWindow.UserName.Text;
+            //Settings.Instance.RegValue.Company = Windows.LoginWindow.Company.Text;
+        }
+
+        private static void OnMainProc()
+        {
+
+            MainProc.Instance.AllProcessStart();
+        }
+    }
+}
